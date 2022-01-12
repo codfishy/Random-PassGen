f1f34c46a1add0ec6218305fb7f105a9275a61d0
Jobin Mathew <72184870+codfishy@users.noreply.github.com>
2022-01-12 23:59:16 +0530

    Update PassGen.py

--- PassGen.py
+++ PassGen.py
@@ -1,6 +1,6 @@
 import random
 
-length=int(input("enter the lenght of the pass:"))
+length=int(input("Password Length:"))
 symbols=['!','@','#','$','%','^','&','*','(',')','<','>','?']
 numbers=['1','2','3','4','5','6','7','8','9','0']
 alpha=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']

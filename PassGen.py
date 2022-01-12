03258a64a9856773f91f2ab7309b3902e9559898
Jobin Mathew <72184870+codfishy@users.noreply.github.com>
2022-01-12 23:57:24 +0530

    Add files via upload

---
+++ PassGen.py
@@ -0,0 +1,16 @@
+import random
+
+length=int(input("enter the lenght of the pass:"))
+symbols=['!','@','#','$','%','^','&','*','(',')','<','>','?']
+numbers=['1','2','3','4','5','6','7','8','9','0']
+alpha=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+alpha_big=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+final_list=alpha+alpha_big+numbers+symbols
+print( "Your Random Password:")
+for i in range(length+1):
+
+    password=random.choice(final_list)
+    print( password, end=' ')
+
+        
+        
